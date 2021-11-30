03f09ae88e89a3ffabd91f959f6d9093790b76a6
Jinho Ko <ben4945@postech.ac.kr>
2021-11-30 19:03:46 +0900

    add uploader

---
+++ uploader.py
@@ -0,0 +1,23 @@
+
+import requests
+
+print("ping database")
+QUERY_LINK = 'http://server1.jinhoko.com:30006'
+response = requests.get(QUERY_LINK + '/ping')
+assert response.status_code == 204, "cannot ping database"
+
+DB_NAME = 'sensor'
+TABLE_NAME = 'pos'
+
+def upload_pos(data):
+
+    ts, beacon, xpos, ypos, prob = data # (423942348, 'B001', 0.5, 3.3, 0.5)
+    qstr = ''
+    qstr += f'{TABLE_NAME},beacon={beacon} x={xpos},y={ypos},prob={prob} {ts} \n'
+    print(qstr)
+
+    res = requests.post(url=QUERY_LINK + f'/write?db={DB_NAME}',
+                        data=qstr,
+                        headers={'Content-Type': 'application/octet-stream'}
+    )
+
