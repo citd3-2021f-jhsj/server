04a63f6a9a1f0f4afec1adbf26c8fadb7b94a8c3
Jinho Ko <ben4945@postech.ac.kr>
2021-12-14 17:46:49 +0900

    .

--- uploader.py
+++ uploader.py
@@ -12,12 +12,16 @@ TABLE_NAME = 'pos'
 def upload_pos(data):
 
     ts, beacon, xpos, ypos, prob = data # (423942348, 'B001', 0.5, 3.3, 0.5)
+    ts = ts * 1000000
     qstr = ''
     qstr += f'{TABLE_NAME},beacon={beacon} x={xpos},y={ypos},prob={prob} {ts} \n'
     print(qstr)
 
-    res = requests.post(url=QUERY_LINK + f'/write?db={DB_NAME}',
+    res = requests.post(url=QUERY_LINK + f'/write?db={DB_NAME}&epoch=us',
                         data=qstr,
                         headers={'Content-Type': 'application/octet-stream'}
     )
+    print(res.status_code)
+    print(res.content)
 
+upload_pos( (1638920603288, 'B111', 0.5, 3.3, 0.5) )
